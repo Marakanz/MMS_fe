208509394faa79a92f0402f14e71db67b36a7bc3
Marakanz <juwonakande07@gmail.com>
2022-01-11 11:00:46 +0100

    admin changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,25 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 import blogReducer from "./slices/blogSlice";
 import menuReducer from "./slices/menuSlice"
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-  } from 'redux-persist'
-  import storage from 'redux-persist/lib/storage'
-
-  const persistConfig = {
-    key: 'root',
-    version: 1,
-    storage,
-  }
-  
-  const persistedReducer = persistReducer(persistConfig, userReducer)
 
 
 export default configureStore({
